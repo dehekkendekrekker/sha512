87a0781ba7fada5a373ff49695689bfeb396e169
dhk <dhk@dehekkendekrekker.nl>
2023-07-30 06:11:19 +0000

    PBKDF passes all tests

--- src/model/python/sha512_rounds.py
+++ src/model/python/sha512_rounds.py
@@ -1,12 +1,18 @@
 from hashlib import sha512
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("--rounds",type=int, required=True)
+args = parser.parse_args()
+rounds = args.rounds
 
 
 input = b"abc"
 algo = sha512()
 
 
-for i in range(2):
-    print("Round: %s Input: %s" % (i, input.hex()))
+for i in range(rounds):
+    print("Round: %s Input: %s" % (i+1, input.hex()))
     input = sha512(input).digest()
 
 print("Result")
